1ca2cfac90a7a5c28769e836769b59e8cec2e22f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-04 13:00:05 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-04

--- Components/Config.cs
+++ Components/Config.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 03 апреля 2026 11:33:43
- * Version: 1.0.7
+ * Last Updated: 04 апреля 2026 13:00:05
+ * Version: 1.0.8
  */
 
 namespace LizeriumFindChanges.Components
