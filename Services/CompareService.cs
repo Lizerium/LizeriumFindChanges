d39f155a41fb8e48ff1c12cd02b552db42002cdf
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-14 09:52:45 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-14

--- Services/CompareService.cs
+++ Services/CompareService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 сентября 2026 06:53:54
- * Version: 1.0.171
+ * Last Updated: 14 сентября 2026 09:52:44
+ * Version: 1.0.172
  */
 
 using LizeriumFindChanges.Components;
